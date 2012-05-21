24ce491042716d420d4113dbfba1d64c220936dc
Daniel Bergløv <webgeek.eu@gmail.com>
2012-05-21 12:07:17 +0200

    Move shared files to the qsd8k vendor repo

--- device-vendor-blobs.mk
+++ device-vendor-blobs.mk
@@ -20,19 +20,14 @@ PRODUCT_COPY_FILES += \
     vendor/htc/passion/proprietary/parse_radio_log:system/bin/parse_radio_log \
     vendor/htc/passion/proprietary/AdieHWCodecSetting.csv:system/etc/AdieHWCodecSetting.csv \
     vendor/htc/passion/proprietary/AudioBTID.csv:system/etc/AudioBTID.csv \
-    vendor/htc/passion/proprietary/bcm4329.hcd:system/etc/firmware/bcm4329.hcd \
     vendor/htc/passion/proprietary/default.acdb:system/etc/firmware/default.acdb \
     vendor/htc/passion/proprietary/default_att.acdb:system/etc/firmware/default_att.acdb \
     vendor/htc/passion/proprietary/default_france.acdb:system/etc/firmware/default_france.acdb \
-    vendor/htc/passion/proprietary/yamato_pfp.fw:system/etc/firmware/yamato_pfp.fw \
-    vendor/htc/passion/proprietary/yamato_pm4.fw:system/etc/firmware/yamato_pm4.fw \
     vendor/htc/passion/proprietary/vpimg:system/etc/vpimg \
-    vendor/htc/passion/proprietary/libgsl.so:system/lib/libgsl.so \
-    vendor/htc/passion/proprietary/libsc-a2xx.so:system/lib/libsc-a2xx.so \
     vendor/htc/passion/proprietary/libhtc_acoustic.so:system/lib/libhtc_acoustic.so \
     vendor/htc/passion/proprietary/libhtc_ril.so:system/lib/libhtc_ril.so \
     vendor/htc/passion/proprietary/libOmxVdec.so:system/lib/libOmxVdec.so \
     vendor/htc/passion/proprietary/mm-venc-omx-test:system/bin/mm-venc-omx-test \
-    vendor/htc/passion/proprietary/libmm-omxcore.so:system/lib/libmm-omxcore.so \
-    vendor/htc/passion/proprietary/libOmxCore.so:system/lib/libOmxCore.so \
-    vendor/htc/passion/proprietary/libOmxVidEnc.so:system/lib/libOmxVidEnc.so
+    vendor/htc/passion/proprietary/libmm-omxcore.so:system/lib/libmm-omxcore.so 
+#vendor/htc/passion/proprietary/libOmxCore.so:system/lib/libOmxCore.so \
+#vendor/htc/passion/proprietary/libOmxVidEnc.so:system/lib/libOmxVidEnc.so
